1c6a7701ce8541496ca438fce14a1db4b134dbd8
Francisco Duarte <francisco.caramez.duarte@gmail.com>
2022-09-07 20:29:52 +0100

    adds placeholder to iamges

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -37,6 +37,7 @@ const ItemPreview = (props) => {
       <img
         alt="item"
         src={item.image}
+        placeholder={item.title}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
